ca5acc860b371824b3417c3ad7f038467071f627
Romero <romero.frank.r@gmail.com>
2017-12-21 19:07:51 +0000

    your comment

---
+++ script.js
@@ -0,0 +1,54 @@
+// onload
+window.onload = function(){
+  
+  var cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  var suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  var randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  var randomSuitNumber = Math.floor(Math.random() * suit.length);
+  var finalSuit = suit[randomSuitNumber];
+  
+  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+  document.getElementById('theCard').className = "";
+  document.getElementById('theCard').classList.add("card");
+  document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
+  
+}
+// click
+window.onclick = function(){
+var cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  var suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  var randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  var randomSuitNumber = Math.floor(Math.random() * suit.length);
+  var finalSuit = suit[randomSuitNumber];
+  
+  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+  document.getElementById('theCard').className = "";
+  document.getElementById('theCard').classList.add("card");
+  document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
+}
+
+// space bar
+ window.onkeyup = function(e){
+    if(e.keyCode == 32){
+        var cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  var suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  var randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  var randomSuitNumber = Math.floor(Math.random() * suit.length);
+  var finalSuit = suit[randomSuitNumber];
+  
+  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+  document.getElementById('theCard').className = "";
+  document.getElementById('theCard').classList.add("card");
+  document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
+    }
+}
+
+function getSuiteClass(suit)
+{
+  switch (suit) {
+    case "Diamonds": return "suit-diamonds"; break;
+    case "Spades": return "suit-spades"; break;
+    case "Hearts": return "suit-hearts"; break;
+    case "Clubs": return "suit-clubs"; break;
+  }
+}
